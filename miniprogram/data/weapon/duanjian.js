a22ecc77edda868b3dd8df1af97ac398783485d1
wangkj <>
2024-09-18 16:09:31 +0800

    remove useless code

--- miniprogram/data/weapon/duanjian.js
+++ miniprogram/data/weapon/duanjian.js
@@ -1,6 +1,12 @@
 export default [{
-  label: '无影剑艾雷诺',
-  icon: 'https://img1.ali213.net/glpic/upload/20200812/21880F39.jpg',
-  level: 50,
-  
+  "icon": "https://img1.ali213.net/glpic/upload/20200812/4D4915C8.jpg",
+  "name": "沧海长风短剑",
+  "type": "短剑",
+  "level": "55",
+  "for": "鬼剑士",
+  "liliang": "",
+  "zhili": "46",
+  "wuligongjili": "魔法攻击力",
+  "mofagongjili": "抗魔值",
+  "affect": "增加30%的技能攻击力，每30秒消耗3个无色小晶块随机应用以下属性之一\n增加10%的物理、魔法攻击力\n增加10%的物理攻击力\n增加10%的魔法攻击力\n所有属性强化+20\n攻击速度+6%\n移动速度+3%\n命中+204"
 }]
